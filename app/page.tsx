7d3ba8ebca78da04984c9cec8c273f790de281cd
ragu47 <ragu.p2100@gmail.com>
2024-03-16 21:10:33 +0530

    commit_changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Connect from "@/Components/Connect";
 import Features from "@/Components/Features";
 import Market from "@/Components/Market";
 import Swap from "@/Components/Swap";
+import 'tailwindcss/tailwind.css'
 
 import Image from "next/image";
 
